f7585045e743fa6a1dd5fb1f011bddb6ed8d3e9d
Jonathan Lancar <jonaphin@gmail.com>
2011-11-22 23:21:44 -0800

    work on disable highlighting / disable rightclick

---
+++ pepperspray-0.1.js
@@ -0,0 +1,41 @@
+(function( $ ) {
+	var settings;
+	var urlHash = window.location.hash;
+	
+	var methods = {
+		init: function( options ) {
+			// Defaults
+			settings = $.extend({
+				'disableRightClick'		: false,
+				'disableHighlighting'	: false,
+			}, options);
+		},
+		disableRightClick: function(state) {
+			state = typeof(state) != 'undefined' ? state : settings["disableRightClick"];
+			$('body').attr("oncontextmenu", "return " + !state + ";");
+		},
+		disableHighlighting: function(state) {
+			state = typeof(state) != 'undefined' ? state : settings["disableHighlighting"];
+			
+			if(state == true) {
+				$("body").bind("mousedown.pepperspray", function(event){
+					event.preventDefault();
+
+					return false;
+				});
+			} else {
+				$("body").unbind("mousedown.pepperspray");
+			}			
+		}
+	};
+	
+  $.fn.pepperspray = function(method) {
+		if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.pepperspray' );
+    }
+  };
+})( jQuery );
