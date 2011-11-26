4115f800b5e9d38a81c185a9b434ee2a80408558
Jonathan Lancar <jonaphin@gmail.com>
2011-11-25 16:30:37 -0800

    back button disabling

--- pepperspray-0.1.js
+++ pepperspray-0.1.js
@@ -1,12 +1,11 @@
-(function( $ ) {
-	var settings;
-	
+(function( $ ) {	
 	var methods = {
 		init: function( options ) {
 			// Defaults
 			settings = $.extend({
 				'disableRightClick'		: false,
 				'disableHighlighting'	: false,
+				'disableBackButton'		: false
 			}, options);
 		},
 		disableRightClick: function(state) {
@@ -25,18 +24,43 @@
 			} else {
 				$("body").unbind("mousedown.pepperspray");
 			}			
+		},
+		disableBackButton: function(state) {
+			state = typeof(state) != 'undefined' ? state : settings["disableBackButton"];
+			settings["disableBackButton"] = state;
+			
+			if(state) {
+				var loc = window.location.href.replace(/#1/g, '') + "#1";
+				window.location.href = loc;
+				$.pepperspray("keepDisablingBackButton");
+			}
+		},
+		keepDisablingBackButton: function() {
+			if(!settings["disableBackButton"])
+				return;
+				
+			if (window.location.hash != "#1")
+				window.location.hash = "#1";
+			
+			setTimeout(function() { $.pepperspray("keepDisablingBackButton") }, 50);
 		}
 	};
 	
+	$.fn.pepperspray = function(method, options) {
+		options = typeof(options) != 'undefined' ? options : {}
+		if(typeof(settings) == 'undefined')
+			methods['init'].apply(this, Array.prototype.slice.call( arguments, 2 ));
+		
+		if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.pepperspray' );
+    }
+  };
+	
 	$.extend({
-		pepperspray: function(method) {
-			if ( methods[method] ) {
-	      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-	    } else if ( typeof method === 'object' || ! method ) {
-	      return methods.init.apply( this, arguments );
-	    } else {
-	      $.error( 'Method ' +  method + ' does not exist on jQuery.pepperspray' );
-	    }
-	  }
+		pepperspray: $.fn.pepperspray
 	});
 })( jQuery );
